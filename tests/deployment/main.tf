6d2c2cde5889a1746808b4b6d84390477414b50a
Lyon Till <lyon.till@microsoft.com>
2020-12-04 17:15:10 +0000

    Update tests/
    Add var input for root name

--- tests/deployment/main.tf
+++ tests/deployment/main.tf
@@ -9,12 +9,16 @@ variable "root_id" {
   type = string
 }
 
+variable "root_name" {
+  type = string
+}
+
 module "enterprise_scale" {
   source = "../../"
 
   root_parent_id = data.azurerm_client_config.current.tenant_id
   root_id        = var.root_id
-  root_name      = "ES"
+  root_name      = var.root_name
 
   deploy_core_landing_zones = true
   deploy_demo_landing_zones = true
