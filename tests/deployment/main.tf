81a4fdf544c88712c564cb76e9cae0b950010467
Lyon Till <lyon.till@microsoft.com>
2020-12-04 12:07:12 +0000

    Add tests/
    Initial commit of test directory

---
+++ tests/deployment/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "=2.38.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+module "enterprise_scale" {
+  source = "../../"
+
+  root_parent_id = data.azurerm_client_config.current.tenant_id
+
+  deploy_core_landing_zones = true
+  deploy_demo_landing_zones = true
+}
