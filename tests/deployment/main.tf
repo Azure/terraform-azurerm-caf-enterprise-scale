c187afba6c3d897e29c31695708f1ce48e891313
Lyon Till <lyon.till@microsoft.com>
2020-12-04 15:57:13 +0000

    Update tests/
    Run file formatter

--- tests/deployment/main.tf
+++ tests/deployment/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = ""
+  version = "2.39"
   features {}
 }
 
@@ -9,8 +9,8 @@ module "enterprise_scale" {
   source = "../../"
 
   root_parent_id = data.azurerm_client_config.current.tenant_id
-  root_id = ""
-  root_name = "ES"
+  root_id        = "xx"
+  root_name      = "ES"
 
   deploy_core_landing_zones = true
   deploy_demo_landing_zones = true
