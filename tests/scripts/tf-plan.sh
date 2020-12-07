d5143a505039f8a2156878948586c20c7adda495
Lyon Till <lyon.till@microsoft.com>
2020-12-07 16:43:08 +0000

    Update scripts/
    Print root id

--- tests/scripts/tf-plan.sh
+++ tests/scripts/tf-plan.sh
@@ -4,9 +4,10 @@ echo "==> Switching directory..."
 cd $PWD/tests/deployment
 
 echo "==> Planning terraform..."
-terraform plan \
-    -var "root_id='$TF_ROOT_ID'" \
-    -var "root_name=ES-'$TF_VERSION'-'$TF_AZ_VERSION'" \
-    -parallelism=256 \
-    -state="./terraform_'$TF_VERSION'_'$TF_AZ_VERSION'.tfstate" \
-    -out="terraform_plan_'$TF_VERSION'_'$TF_AZ_VERSION'"
+echo "==> Root ID - $TF_ROOT_ID"
+# terraform plan \
+#     -var "root_id='$TF_ROOT_ID'" \
+#     -var "root_name=ES-'$TF_VERSION'-'$TF_AZ_VERSION'" \
+#     -parallelism=256 \
+#     -state="./terraform_'$TF_VERSION'_'$TF_AZ_VERSION'.tfstate" \
+#     -out="terraform_plan_'$TF_VERSION'_'$TF_AZ_VERSION'"
