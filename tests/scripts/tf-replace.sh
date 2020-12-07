79f2efcbbfa0e3cd5439a3c2ac99d894f587c812
Lyon Till <lyon.till@microsoft.com>
2020-12-07 16:29:35 +0000

    Update scripts/
    Add escape characters

--- tests/scripts/tf-replace.sh
+++ tests/scripts/tf-replace.sh
@@ -6,4 +6,4 @@ cd $PWD/tests/deployment
 echo "==> Provider version - $TF_AZ_VERSION"
 
 #echo "==> Updating provider version..."
-#sed -i 's/version = ""/version = "$(TF_AZ_VERSION)"/g' main.tf
+sed -i 's/version = ""/version = "'$(TF_AZ_VERSION)'"/g' main.tf
