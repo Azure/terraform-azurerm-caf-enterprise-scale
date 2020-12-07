cdd21ece1acc7505b2b1b16e2e4fb176ecbbeb58
Lyon Till <lyon.till@microsoft.com>
2020-12-07 16:25:44 +0000

    Update scripts/
    Print provider version

--- tests/scripts/tf-replace.sh
+++ tests/scripts/tf-replace.sh
@@ -3,5 +3,7 @@
 echo "==> Switching directory..."
 cd $PWD/tests/deployment
 
-echo "==> Updating provider version..."
-sed -i 's/version = ""/version = "$(TF_AZ_VERSION)"/g' main.tf
+echo "==> Provider version - $TF_AZ_VERSION"
+
+#echo "==> Updating provider version..."
+#sed -i 's/version = ""/version = "$(TF_AZ_VERSION)"/g' main.tf
