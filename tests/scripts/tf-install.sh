ce0bb34b0c67dae1cecdd7f06afb348c54cef43b
Lyon Till <lyon.till@microsoft.com>
2020-12-07 18:03:49 +0000

    Update scripts/
    Update binary install path

--- tests/scripts/tf-install.sh
+++ tests/scripts/tf-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+echo "==> Creating directory..."
+mkdir -p /home/runner/.terraform/bin/
+
 echo "==> Downloading archive..."
 wget 'https://releases.hashicorp.com/terraform/0.14.0/terraform_'$TF_VERSION'_linux_amd64.zip' -P /tmp
 
@@ -7,4 +10,7 @@ echo "==> Expanding archive..."
 unzip '/tmp/terraform_'$TF_VERSION'_linux_amd64.zip' -d /tmp
 
 echo "==> Moving binaries..."
-mv /tmp/terraform /usr/local/bin/
+mv /tmp/terraform /home/runner/.terraform/bin/terraform
+
+echo "==> Exporting path..."
+echo "/home/runner/.terraform/bin" >> $GITHUB_PATH
