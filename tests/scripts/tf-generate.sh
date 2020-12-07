51ee95e45543f69247c53c71330059df6aa76717
Lyon Till <lyon.till@microsoft.com>
2020-12-07 18:18:22 +0000

    Update scripts/
    Merge generate and replace into prepare step

--- tests/scripts/tf-generate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-echo "==> Generating root id..."
-echo "TF_ROOT_ID=$RANDOM" >> $GITHUB_ENV
--- tests/scripts/tf-prepare.sh
+++ tests/scripts/tf-prepare.sh
@@ -3,6 +3,9 @@
 echo "==> Switching directories..."
 cd $PWD/tests/deployment
 
+echo "==> Generating root id..."
+echo "TF_ROOT_ID=$RANDOM" >> $GITHUB_ENV
+
 echo "==> Replacing provider version..."
 echo "==> Provider version - $TF_AZ_VERSION"
 sed -i 's/version = ""/version = "'$TF_AZ_VERSION'"/g' main.tf
