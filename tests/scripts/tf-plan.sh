60f923e192a459f0fb57881fef73fff903d6f999
Lyon Till <lyon.till@microsoft.com>
2020-12-07 16:46:02 +0000

    Update scripts/
    Fix environment variable names

--- tests/scripts/tf-plan.sh
+++ tests/scripts/tf-plan.sh
@@ -5,9 +5,9 @@ cd $PWD/tests/deployment
 
 echo "==> Planning terraform..."
 echo "==> Root ID - $TF_ROOT_ID"
-# terraform plan \
-#     -var "root_id='$TF_ROOT_ID'" \
-#     -var "root_name=ES-'$TF_VERSION'-'$TF_AZ_VERSION'" \
-#     -parallelism=256 \
-#     -state="./terraform_'$TF_VERSION'_'$TF_AZ_VERSION'.tfstate" \
-#     -out="terraform_plan_'$TF_VERSION'_'$TF_AZ_VERSION'"
+terraform plan \
+    -var "root_id=$TF_ROOT_ID" \
+    -var "root_name=ES-$TF_VERSION-$TF_AZ_VERSION" \
+    -parallelism=256 \
+    -state="./terraform-$TF_VERSION-$TF_AZ_VERSION.tfstate" \
+    -out="terraform-plan-$TF_VERSION-$TF_AZ_VERSION"
