c61080a1bd31b9010861cf7c1cfb2532908956b5
Lyon Till <lyon.till@microsoft.com>
2021-01-13 16:42:27 +0000

    Update scripts/
    Add generated root id output

--- tests/scripts/tf-prepare.sh
+++ tests/scripts/tf-prepare.sh
@@ -9,9 +9,15 @@ echo "==> Switching directories..."
 cd $PWD/tests/deployment
 
 echo "==> Generating root id's..."
-echo "TF_ROOT_ID_1=$RANDOM" >> $GITHUB_ENV
-echo "TF_ROOT_ID_2=$RANDOM" >> $GITHUB_ENV
-echo "TF_ROOT_ID_3=$RANDOM" >> $GITHUB_ENV
+ROOT_ID_1=$RANDOM
+echo "==> Azure Root ID 1 - $ROOT_ID_1"
+echo "TF_ROOT_ID_1=$ROOT_ID_1" >> $GITHUB_ENV
+ROOT_ID_2=$RANDOM
+echo "==> Azure Root ID 2 - $ROOT_ID_2"
+echo "TF_ROOT_ID_2=$ROOT_ID_2" >> $GITHUB_ENV
+ROOT_ID_3=$RANDOM
+echo "==> Azure Root ID 3 - $ROOT_ID_3"
+echo "TF_ROOT_ID_3=$ROOT_ID_3" >> $GITHUB_ENV
 
 echo "==> Replacing provider version..."
 sed -i 's/version = ""/version = "'$TF_AZ_VERSION'"/g' main.tf
