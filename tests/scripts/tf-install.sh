33a68935a55d1e9f3c31fa89b9cd32cdbce785c3
Lyon Till <lyon.till@microsoft.com>
2021-02-03 16:24:22 +0000

    Update tests/
    Fix install scm bug

--- tests/scripts/tf-install.sh
+++ tests/scripts/tf-install.sh
@@ -6,17 +6,42 @@ set -e
 # - Terraform Install
 #
 
-echo "==> Creating directory..."
-mkdir -p /home/runner/.terraform/bin/
+case $SCM in
+    'AzurePipelines')
+        echo "==> Creating directory..."
+        mkdir -p /home/runner/.terraform/bin/
 
-echo "==> Downloading archive..."
-wget 'https://releases.hashicorp.com/terraform/'$TF_VERSION'/terraform_'$TF_VERSION'_linux_amd64.zip' -P /tmp
+        echo "==> Downloading archive..."
+        wget 'https://releases.hashicorp.com/terraform/'$TF_VERSION'/terraform_'$TF_VERSION'_linux_amd64.zip' -P /tmp
+        
+        echo "==> Expanding archive..."
+        unzip '/tmp/terraform_'$TF_VERSION'_linux_amd64.zip' -d /tmp
+        
+        echo "==> Moving binaries..."
+        mv /tmp/terraform /home/vsts/.terraform/bin/terraform
+        
+        echo "==> Exporting path..."
+        echo "##vso[task.prependpath]/home/vsts/.terraform/bin"
+        ;;
+    'GitHubActions')
+        echo "==> Creating directory..."
+        mkdir -p /home/runner/.terraform/bin/
+        
+        echo "==> Downloading archive..."
+        wget 'https://releases.hashicorp.com/terraform/'$TF_VERSION'/terraform_'$TF_VERSION'_linux_amd64.zip' -P /tmp
+        
+        echo "==> Expanding archive..."
+        unzip '/tmp/terraform_'$TF_VERSION'_linux_amd64.zip' -d /tmp
+        
+        echo "==> Moving binaries..."
+        mv /tmp/terraform /home/runner/.terraform/bin/terraform
+        
+        echo "==> Exporting path..."
+        echo "/home/runner/.terraform/bin" >> $GITHUB_PATH
+        ;;
+    *)
+        echo "==> Unsupported platform..."
+        exit 1
+        ;;
+esac
 
-echo "==> Expanding archive..."
-unzip '/tmp/terraform_'$TF_VERSION'_linux_amd64.zip' -d /tmp
-
-echo "==> Moving binaries..."
-mv /tmp/terraform /home/runner/.terraform/bin/terraform
-
-echo "==> Exporting path..."
-echo "/home/runner/.terraform/bin" >> $GITHUB_PATH
