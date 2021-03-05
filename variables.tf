ba739702b50b229d667f20972edc7256d17cd01a
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-03-05 21:31:26 +0000

    Update root_parent_id validation regex

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "root_parent_id" {
   description = "The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones."
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9-]{1,36}$", var.root_parent_id))
+    condition     = can(regex("^[a-zA-Z0-9-_\\(\\)\\.]{1,36}$", var.root_parent_id))
     error_message = "The root_parent_id value must be a valid GUID, or Management Group ID."
   }
 }
