a52f9e1bb271f865897050efbc4f87379bcc9987
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-04-09 21:01:15 +0100

    Fix terraform fmt

--- resources.role_assignments.tf
+++ resources.role_assignments.tf
@@ -10,8 +10,8 @@ resource "azurerm_role_assignment" "enterprise_scale" {
   principal_id = each.value.principal_id
 
   # Optional attributes
-  role_definition_name             = try(each.value.role_definition_name, null)
-  role_definition_id               = try(each.value.role_definition_id, null)
+  role_definition_name = try(each.value.role_definition_name, null)
+  role_definition_id   = try(each.value.role_definition_id, null)
 
   # Set explicit dependency on Management Group, Policy, and Role Definition deployments
   depends_on = [
