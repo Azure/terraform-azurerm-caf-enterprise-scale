813dc36338284784bc0d02791176376e21a0c67d
J0hn-B <v-jbenakis@microsoft.com>
2021-04-26 14:39:26 +0100

    add-remove conf-settings from built-in archetypes

--- modules/archetypes/locals.archetype_definitions.tf
+++ modules/archetypes/locals.archetype_definitions.tf
@@ -10,6 +10,18 @@ locals {
   custom_archetype_definitions_yaml = local.custom_library_path_specified ? tolist(fileset(local.custom_library_path, "**/archetype_definition_*.{yml,yaml}")) : []
 }
 
+# Load the archetype definition extensions
+locals {
+  extend_archetype_definitions_json = local.custom_library_path_specified ? tolist(fileset(local.custom_library_path, "**/archetype_extension_*.json")) : []       ##issue_72
+  extend_archetype_definitions_yaml = local.custom_library_path_specified ? tolist(fileset(local.custom_library_path, "**/archetype_extension_*.{yml,yaml}")) : [] ##issue_72
+}
+
+# Load the archetype definition exclusions
+locals {
+  exclude_archetype_definitions_json = local.custom_library_path_specified ? tolist(fileset(local.custom_library_path, "**/archetype_exclusion_*.json")) : []       ##issue_72
+  exclude_archetype_definitions_yaml = local.custom_library_path_specified ? tolist(fileset(local.custom_library_path, "**/archetype_exclusion_*.{yml,yaml}")) : [] ##issue_72
+}
+
 # Create datasets containing all built-in and custom archetype definitions from each source and file type
 locals {
   builtin_archetype_definitions_dataset_from_json = {
@@ -27,6 +39,22 @@ locals {
   custom_archetype_definitions_dataset_from_yaml = {
     for filepath in local.custom_archetype_definitions_yaml :
     filepath => yamldecode(templatefile("${local.custom_library_path}/${filepath}", local.template_file_vars))
+  }
+    extend_archetype_definitions_dataset_from_json = { ##issue_72
+    for filepath in local.extend_archetype_definitions_json :
+    filepath => jsondecode(templatefile("${local.custom_library_path}/${filepath}", local.template_file_vars))
+  }
+  extend_archetype_definitions_dataset_from_yaml = { ##issue_72
+    for filepath in local.extend_archetype_definitions_yaml :
+    filepath => jsondecode(templatefile("${local.custom_library_path}/${filepath}", local.template_file_vars))
+  }
+  exclude_archetype_definitions_dataset_from_json = { ##issue_72
+    for filepath in local.exclude_archetype_definitions_json :
+    filepath => jsondecode(templatefile("${local.custom_library_path}/${filepath}", local.template_file_vars))
+  }
+  exclude_archetype_definitions_dataset_from_yaml = { ##issue_72
+    for filepath in local.exclude_archetype_definitions_yaml :
+    filepath => jsondecode(templatefile("${local.custom_library_path}/${filepath}", local.template_file_vars))
   }
 }
 
@@ -48,6 +76,22 @@ locals {
     for key, value in local.custom_archetype_definitions_dataset_from_yaml :
     keys(value)[0] => values(value)[0]
   }
+    extend_archetype_definitions_map_from_json = { ##issue_72
+    for key, value in local.extend_archetype_definitions_dataset_from_json :
+    keys(value)[0] => values(value)[0]
+  }
+  extend_archetype_definitions_map_from_yaml = { ##issue_72
+    for key, value in local.extend_archetype_definitions_dataset_from_yaml :
+    keys(value)[0] => values(value)[0]
+  }
+  exclude_archetype_definitions_map_from_json = { ##issue_72
+    for key, value in local.exclude_archetype_definitions_dataset_from_json :
+    keys(value)[0] => values(value)[0]
+  }
+  exclude_archetype_definitions_map_from_yaml = { ##issue_72
+    for key, value in local.exclude_archetype_definitions_dataset_from_yaml :
+    keys(value)[0] => values(value)[0]
+  }
 }
 
 # Merge the archetype maps into a single map, and extract the desired archetype definition.
@@ -55,12 +99,80 @@ locals {
 # defined to override a built-in definition, this is handled by
 # merging the custom archetypes after the built-in archetypes.
 locals {
-  archetype_definitions = merge(
+  base_archetype_definitions = merge(
     local.builtin_archetype_definitions_map_from_json,
     local.builtin_archetype_definitions_map_from_yaml,
     local.custom_archetype_definitions_map_from_json,
     local.custom_archetype_definitions_map_from_yaml,
   )
+}
+
+# Merge the extend archetype maps into a single map.
+locals {
+  extend_archetype_definitions = merge(
+    local.extend_archetype_definitions_map_from_json, ##issue_72 
+    local.extend_archetype_definitions_map_from_yaml, ##issue_72 
+  )
+}
+
+# Merge the exclude archetype maps into a single map.
+locals {
+  exclude_archetype_definitions = merge(
+    local.exclude_archetype_definitions_map_from_json, ##issue_72 
+    local.exclude_archetype_definitions_map_from_yaml, ##issue_72 
+  )
+}
+
+# Add or remove configuration settings from an existing [built-in] archetype definition.
+# Get full description context in github #issue_72
+locals {
+  archetype_definitions = {
+    for adk, adv in local.base_archetype_definitions :
+    adk => {
+      policy_assignments = [
+        for value in distinct(concat(
+          adv.policy_assignments,
+          try(local.extend_archetype_definitions["extend_${adk}"].policy_assignments, local.empty_list)
+        )) : value
+        if contains(try(local.exclude_archetype_definitions["exclude_${adk}"].policy_assignments, local.empty_list), value) != true
+      ],
+      policy_definitions = [
+        for value in distinct(concat(
+          adv.policy_definitions,
+          try(local.extend_archetype_definitions["extend_${adk}"].policy_definitions, local.empty_list)
+        )) : value
+        if contains(try(local.exclude_archetype_definitions["exclude_${adk}"].policy_definitions, local.empty_list), value) != true
+      ],
+      policy_set_definitions = [
+        for value in distinct(concat(
+          adv.policy_set_definitions,
+          try(local.extend_archetype_definitions["extend_${adk}"].policy_set_definitions, local.empty_list)
+        )) : value
+        if contains(try(local.exclude_archetype_definitions["exclude_${adk}"].policy_set_definitions, local.empty_list), value) != true
+      ],
+      role_definitions = [
+        for value in distinct(concat(
+          adv.role_definitions,
+          try(local.extend_archetype_definitions["extend_${adk}"].role_definitions, local.empty_list)
+        )) : value
+        if contains(try(local.exclude_archetype_definitions["exclude_${adk}"].role_definitions, local.empty_list), value) != true
+      ],
+      archetype_config = {
+        parameters = merge(
+          adv.archetype_config.parameters,
+          try(local.extend_archetype_definitions["extend_${adk}"].archetype_config.parameters, local.empty_map)
+        )
+        access_control = merge(
+          adv.archetype_config.access_control,
+          try(local.extend_archetype_definitions["extend_${adk}"].archetype_config.access_control, local.empty_map)
+        )
+      }
+    }
+  }
+}
+
+# Extract the required archetype_definition value for the current context
+locals {
   archetype_definition = local.archetype_definitions[local.archetype_id]
 }
 
