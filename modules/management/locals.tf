8645e1da08f7b7f566a39fe466a5c0b90e381193
Tobias Keitsch <tobias.keitsch@zenseact.com>
2021-05-12 00:27:15 +0200

    reuse la rg for asc export

--- modules/management/locals.tf
+++ modules/management/locals.tf
@@ -185,7 +185,7 @@ locals {
         Deploy-ASC-Defender = {
           emailSecurityContact                = local.settings.security_center.config.email_security_contact
           logAnalytics                        = local.log_analytics_workspace_resource_id
-          ascExportResourceGroupName          = "${local.root_id}-asc-export"
+          ascExportResourceGroupName          = local.azurerm_log_analytics_workspace.resource_group_name
           ascExportResourceGroupLocation      = local.location
           pricingTierContainerRegistry        = local.deploy_defender_for_acr ? "Standard" : "Free"
           pricingTierAppServices              = local.deploy_defender_for_app_services ? "Standard" : "Free"
@@ -200,11 +200,9 @@ locals {
         }
         Deploy-LX-Arc-Monitoring = {
           logAnalytics = local.log_analytics_workspace_resource_id
-
         }
         Deploy-VM-Monitoring = {
           logAnalytics_1 = local.log_analytics_workspace_resource_id
-
         }
         Deploy-VMSS-Monitoring = {
           logAnalytics_1 = local.log_analytics_workspace_resource_id
