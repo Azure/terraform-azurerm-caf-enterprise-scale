cc6970325023cc399a8a5291ea9fb56dffecdf12
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-05-25 10:17:01 +0100

    Update comments for solutions depends_on block

--- resources.management.tf
+++ resources.management.tf
@@ -45,7 +45,10 @@ resource "azurerm_log_analytics_solution" "enterprise_scale" {
   # Optional resource attributes
   tags = each.value.template.tags
 
-  # Set explicit dependency on Resource Group and Log Analytics workspace deployments
+  # Set explicit dependency on Resource Group, Log Analytics
+  # workspace and Automation Account to fix issue #109.
+  # Ideally we would limit to specific solutions, but the
+  # depends_on block only supports static values.
   depends_on = [
     azurerm_resource_group.enterprise_scale,
     azurerm_log_analytics_workspace.enterprise_scale,
