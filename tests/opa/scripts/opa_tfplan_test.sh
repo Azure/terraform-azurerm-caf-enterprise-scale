d3d8f54feab845879df66dab390775c138d123bc
J0hn-B <v-jbenakis@microsoft.com>
2021-06-04 09:18:25 +0100

    update bash script

--- tests/opa/scripts/opa_tfplan_test.sh
+++ tests/opa/scripts/opa_tfplan_test.sh
@@ -12,7 +12,7 @@ VERSION=v4.9.3
 BINARY=yq_linux_amd64
 
 # Install Conftest on Linux:
-if [ $(command -v conftest) ]; then
+if [ $"(command -v conftest)" ]; then
     echo "--> Conftest exists, skip install"
 else
     wget https://github.com/open-policy-agent/conftest/releases/download/v0.24.0/conftest_0.24.0_Linux_x86_64.tar.gz
@@ -22,7 +22,7 @@ else
 fi
 
 # # Install yq on Linux:
-if [ $(command -v yq) ]; then
+if [ $"(command -v yq)" ]; then
     echo "--> yq exists, skip install"
 else
     wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq &&
@@ -44,10 +44,10 @@ rm $PLAN_NAME.test
 
 # # Extract the planned values from the tfplan.json in a temporary *.json file.
 # # To avoid an error from Conftest, "bad yml type", use '.planned_values.root_module' with jq.
-cat opa.json | jq '.planned_values.root_module' >planned_values.json
+cmd <file | jq '.planned_values.root_module' >planned_values.json
 
 # # Convert to yaml.
-cat planned_values.json | yq e -P - | tee policy/planned_values.yml
+cmd <file | yq e -P - | tee policy/planned_values.yml
 
 # # Delete the *.json after converting to *.yml
 rm planned_values.json
