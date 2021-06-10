0dc808448f7468b2151fa0282e93bd66eebf11e2
J0hn-B <v-jbenakis@microsoft.com>
2021-06-10 15:36:01 +0100

    update vars

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -13,12 +13,12 @@ cd ../deployment && terraform show -json "$TF_PLAN_JSON" >"$TF_PLAN_JSON".json
 
 echo "==> Load planned values..."
 cd ../opa/policy &&
-    <planned_values_template.yml
-sed -e 's:root-id-1:'"${root_id_1}"':g' \
-    -e's:root-id-2:'"${root_id_2}"':g' \
-    -e 's:root-id-3:'"${root_id_3}"':g' \
-    -e 's:root-name:'"${root_name}"':g' \
-    -e's:eastus:'"${location}"':g' >planned_values.yml
+    cat <planned_values_template.yml |
+    sed -e 's:root-id-1:'"${ROOT_ID_1}"':g' \
+        -e's:root-id-2:'"${ROOT_ID_2}"':g' \
+        -e 's:root-id-3:'"${ROOT_ID_3}"':g' \
+        -e 's:root-name:'"${ROOT_NAME}"':g' \
+        -e's:eastus:'"${LOCATION}"':g' >planned_values.yml
 
 echo "==> Running conftest..."
 cd ../../deployment &&
