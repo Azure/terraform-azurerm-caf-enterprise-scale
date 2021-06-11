9ca3dda050108ed34daaf16b46bf97a00c76335d
J0hn-B <v-jbenakis@microsoft.com>
2021-06-11 18:11:15 +0100

    sed var test

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -10,9 +10,9 @@ TF_PLAN_JSON="terraform-plan-$TF_VERSION-$TF_AZ_VERSION"
 
 echo "==> Load planned values..."
 cd "$PIPELINE_WORKSPACE/s/tests/opa/policy" &&
-    sed -e 's:root-id-1:'"\"${TF_ROOT_ID_1}"\"':g' \
-        -e's:root-id-2:'"\"${TF_ROOT_ID_2}"\"':g' \
-        -e 's:root-id-3:'"${TF_ROOT_ID_3}"':g' \
+    sed -e 's:root-id-1:"'"${TF_ROOT_ID_1}"'":g' \
+        -e's:root-id-2:"'"${TF_ROOT_ID_2}"'":g' \
+        -e 's:root-id-3:"'"${TF_ROOT_ID_3}"'":g' \
         -e 's:root-name:'"ES-${TF_VERSION}-${TF_AZ_VERSION}"':g' \
         -e's:eastus:'"${DEFAULT_LOCATION}"':g' planned_values_template.yml |
     tee "$TF_PLAN_JSON"_planned_values.yml
