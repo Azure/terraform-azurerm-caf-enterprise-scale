97b10727d5824322d83a7137fcafc0473a73d21e
J0hn-B <v-jbenakis@microsoft.com>
2021-06-16 11:27:27 +0100

    chack tfvars

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -16,9 +16,9 @@ sed -e 's:root-id-1:'"$ROOT_ID_1"':g' \
     -e 's:root-name:'"ES-${TF_VERSION}-${TF_AZ_VERSION}"':g' \
     -e 's:eastus:'"$LOCATION"':g' planned_values_template.yml >"$TF_PLAN_JSON"_planned_values.yml
 
-echo "==> Azure Root ID 1 - $(ROOT_ID_1)"
-echo "==> Azure Root ID 1 - $(ROOT_ID_2)"
-echo "==> Azure Root ID 1 - $(ROOT_ID_3)"
+echo "==> Azure Root ID 1 - $TF_ROOT_ID_1"
+echo "==> Azure Root ID 1 - $TF_ROOT_ID_2"
+echo "==> Azure Root ID 1 - $TF_ROOT_ID_3"
 
 echo "==> Update root-id values..."
 id_1=$ROOT_ID_1 id_2=$ROOT_ID_2 id_3=$ROOT_ID_3 yq eval '(.child_modules[].resources[] |= select(.address == "*root_id_1*").values.name = strenv(id_1)) | (.child_modules[].resources[] |= select(.address == "*root_id_2*").values.name = strenv(id_2))| (.child_modules[].resources[] |= select(.address == "*root_id_3*").values.name = strenv(id_3))' -i "$TF_PLAN_JSON"_planned_values.yml
