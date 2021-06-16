21bd1d701513d732eb06fb65e27aa71c64d0da1e
J0hn-B <v-jbenakis@microsoft.com>
2021-06-16 11:32:43 +0100

    add tfvars

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -21,7 +21,7 @@ echo "==> Azure Root ID 1 - $TF_ROOT_ID_2"
 echo "==> Azure Root ID 1 - $TF_ROOT_ID_3"
 
 echo "==> Update root-id values..."
-id_1=$ROOT_ID_1 id_2=$ROOT_ID_2 id_3=$ROOT_ID_3 yq eval '(.child_modules[].resources[] |= select(.address == "*root_id_1*").values.name = strenv(id_1)) | (.child_modules[].resources[] |= select(.address == "*root_id_2*").values.name = strenv(id_2))| (.child_modules[].resources[] |= select(.address == "*root_id_3*").values.name = strenv(id_3))' -i "$TF_PLAN_JSON"_planned_values.yml
+id_1=$TF_ROOT_ID_1 id_2=$TF_ROOT_ID_2 id_3=$TF_ROOT_ID_3 yq eval '(.child_modules[].resources[] |= select(.address == "*root_id_1*").values.name = strenv(id_1)) | (.child_modules[].resources[] |= select(.address == "*root_id_2*").values.name = strenv(id_2))| (.child_modules[].resources[] |= select(.address == "*root_id_3*").values.name = strenv(id_3))' -i "$TF_PLAN_JSON"_planned_values.yml
 
 echo "==> Test Conftest..."
 which conftest
