2b4bcf58a8f09d43939171e1f58d02423509283b
J0hn-B <v-jbenakis@microsoft.com>
2021-06-17 11:39:43 +0100

    update vars

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -13,7 +13,7 @@ cd "$PIPELINE_WORKSPACE/s/tests/opa/policy"
 sed -e 's:root-id-1:'"$TF_ROOT_ID_1"':g' \
     -e 's:root-id-2:'"$TF_ROOT_ID_2"':g' \
     -e 's:root-id-3:'"$TF_ROOT_ID_3"':g' \
-    -e 's:root-name:'"ES-${TF_VERSION}-${TF_AZ_VERSION}"':g' \
+    -e 's:root-name:ES-'"$TF_VERSION"'-'"$TF_AZ_VERSION"':g' \
     -e 's:eastus:'"$LOCATION"':g' planned_values_template.yml >"$TF_PLAN_JSON"_planned_values.yml
 
 echo "==> Azure Root ID 1 - $ROOT_ID_1"
