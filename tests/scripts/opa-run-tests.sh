b7a376b74180bb6b0033c6a9fa05156b70d34e70
J0hn-B <v-jbenakis@microsoft.com>
2021-06-17 12:05:57 +0100

    tail yml file

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -27,8 +27,12 @@ conftest
 
 wait
 
-echo "==> Check yaml for errors..."
-yamllint -d relaxed "$TF_PLAN_JSON"_planned_values.yml
+echo "==> Print first and last 100 lines of planned values...\n"
+head --lines 100 "$TF_PLAN_JSON"_planned_values.yml
+echo "..."
+tail --lines 100 "$TF_PLAN_JSON"_planned_values.yml
+# echo "==> Check yaml for errors..."
+# yamllint -d relaxed "$TF_PLAN_JSON"_planned_values.yml
 
 echo "==> Running conftest..."
 cd "$PIPELINE_WORKSPACE/s/tests/deployment"
