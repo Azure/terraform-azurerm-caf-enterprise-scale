e4676d810ff8c6fee6ea033b69012b982699f675
J0hn-B <v-jbenakis@microsoft.com>
2021-06-22 12:33:14 +0100

    add policy_set_definition

--- tests/scripts/opa-run-tests.sh
+++ tests/scripts/opa-run-tests.sh
@@ -39,9 +39,16 @@ echo "==> Running conftest..."
 echo
 echo "==> Testing management_groups..."
 conftest test "$TF_PLAN_JSON".json -p ../opa/policy/management_groups.rego -d ../opa/policy/"$TF_PLAN_JSON"_updated_planned_values.yml
+wait
 echo
 echo "==> Testing role_definitions..."
 conftest test "$TF_PLAN_JSON".json -p ../opa/policy/role_definitions.rego -d ../opa/policy/"$TF_PLAN_JSON"_updated_planned_values.yml
+wait
 echo
 echo "==> Testing role_assignments..."
 conftest test "$TF_PLAN_JSON".json -p ../opa/policy/role_assignments.rego -d ../opa/policy/"$TF_PLAN_JSON"_updated_planned_values.yml
+wait
+echo
+echo "==> Testing policy_set_definitions..."
+conftest test "$TF_PLAN_JSON".json -p ../opa/policy/policy_set_definitions.rego -d ../opa/policy/"$TF_PLAN_JSON"_updated_planned_values.yml
+wait
