9c5bf8e8741c1a5da6b2f5b5dddd76bc5adce26c
J0hn-B <v-jbenakis@microsoft.com>
2021-06-23 16:18:13 +0100

    Update pwsh script

--- tests/scripts/opa-values-generator.ps1
+++ tests/scripts/opa-values-generator.ps1
@@ -8,62 +8,64 @@ $PLAN_NAME = "terraform-plan"
 
 
 # Install Scoop
-if (Get-command scoop) {
-    Write-Information "==> Scoop exists, skip install"
+if (Get-command -name scoop) {
+    Write-Output "==> Scoop exists, skip install"
     scoop --version
 }
 else {
-    Write-Information "==> Install Scoop on Windows..."
-    #Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+    Write-Output "==> To install Scoop on Windows, run this command from a new terminal:"
+    Write-Output "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https:\\get.scoop.sh')"
+    
 }
 
 # Install Terraform
 if (Get-command -name terraform -ErrorAction SilentlyContinue) {
-    Write-Information "==> Terraform exists, skip install"
+    Write-Output "==> Terraform exists, skip install"
     terraform version
 }
 else {
-    Write-Information "==> Install Terraform on Windows..."
+    Write-Output "==> Install Terraform on Windows..."
     scoop install terraform
 }
 
 # Install jq
 if (Get-command -name jq -ErrorAction SilentlyContinue) {
-    Write-Information "==> jq exists, skip install"
+    Write-Output "==> jq exists, skip install"
     jq --version
 }
 else {
-    Write-Information "==> Install jq on Windows..."
+    Write-Output "==> Install jq on Windows..."
     scoop install jq
 }
 
 # Install yq
 if (Get-command -name yq -ErrorAction SilentlyContinue) {
-    Write-Information "==> yq exists, skip install"
+    Write-Output "==> yq exists, skip install"
     yq --version
 }
 else {
-    Write-Information "==> Install yq on Windows..."
+    Write-Output "==> Install yq on Windows..."
     scoop install yq
 }
 
 # Install Conftest
 if (Get-command -name conftest -ErrorAction SilentlyContinue) {
-    Write-Information "==> conftest exists, skip install"
+    Write-Output "==> conftest exists, skip install"
     conftest --version
 }
 else {
-    Write-Information "==> Install conftest on Windows..."
+    Write-Output "==> Install conftest on Windows..."
+    scoop bucket add instrumenta https:\\github.com\instrumenta\scoop-instrumenta
     scoop install conftest
 }
 
-Write-Information "==> Change to the module root directory..."
+Write-Output "==> Change to the module root directory..."
 Set-Location ..\deployment
 
-Write-Information "==> Initializing infrastructure..."
+Write-Output "==> Initializing infrastructure..."
 terraform init
 
-Write-Information "==> Planning infrastructure..."
+Write-Output "==> Planning infrastructure..."
 terraform plan `
     -var="root_id_1=root-id-1" `
     -var="root_id_2=root-id-2" `
@@ -72,28 +74,51 @@ terraform plan `
     -var="location=eastus" `
     -out="$PLAN_NAME"
 
-Write-Information "==> Converting plan to *.json..."
+Write-Output "==> Converting plan to *.json..."
 terraform show -json $PLAN_NAME | Out-File -FilePath .\$PLAN_NAME.json
 
-Write-Information "==> Removing the original plan..."
+Write-Output "==> Removing the original plan..."
 Remove-Item -Path .\$PLAN_NAME
 
-Write-Information "==> Saving planned values to a temporary planned_values.json..."
+Write-Output "==> Saving planned values to a temporary planned_values.json..."
 Get-Content -Path .\$PLAN_NAME.json | jq '.planned_values.root_module' | Out-File -FilePath .\planned_values.json
 
-Write-Information "==> Converting to yaml..."
-Get-Content -Path .\planned_values.json | yq e -P - | Tee-Object ..\opa\policy\planned_values_template.yml
+Write-Output "==> Converting to yaml..."
+Get-Content -Path .\planned_values.json | yq e -P - | Tee-Object ..\opa\policy\planned_values.yml
 
-Write-Information "==> Removing the temporary planned_values.json..."
-Remove-Item -Path .\planned_values.json
 
-$CONFIRM = Read-Host "Do you want to remove terraform-plan.json (y/n)?"
+# # #  Run OPA Tests
+Set-Location ..\deployment
+Write-Output "==> Running conftest..."
+
+Write-Output "==> Testing management_groups..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\management_groups.rego -d ..\opa\policy\planned_values.yml
+
+Write-Output "==> Testing role_definitions..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\role_definitions.rego -d ..\opa\policy\planned_values.yml
+
+Write-Output "==> Testing role_assignments..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\role_assignments.rego -d ..\opa\policy\planned_values.yml
+
+Write-Output "==> Testing policy_set_definitions..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\policy_set_definitions.rego -d ..\opa\policy\planned_values.yml
+
+Write-Output "==> Testing policy_definitions..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\policy_definitions.rego -d ..\opa\policy\planned_values.yml
+
+Write-Output "==> Testing policy_assignments..."
+conftest test "$PLAN_NAME.json" -p ..\opa\policy\policy_assignments.rego -d ..\opa\policy\planned_values.yml
+
+$CONFIRM = Read-Host "Do you want to prepare files for repository (y/n)?"
 if ($CONFIRM -eq 'y') {
-    Remove-Item -Path .\terraform-plan.json
+    Remove-Item -Path .\$PLAN_NAME.json
     Write-Output "$PLAN_NAME.json has been removed from your root module"
+    Remove-Item -Path ..\opa\policy\planned_values.yml
+    Write-Output "planned_values.yml has been removed from your \opa\policy\ directory"
 }
 else {
     Write-Warning -Message "$PLAN_NAME.json  can contain sensitive data"
     Write-Warning -Message  "Exposing $PLAN_NAME.json in a repository can cause security breach"
-    Write-Output "From within your terraform root module: conftest test $PLAN_NAME.json -p ../opa/policy/  -d ../opa/policy/planned_values_template.yml"
+    Write-Output "From within your terraform root module: conftest test $PLAN_NAME.json -p ..\opa\policy\  -d ..\opa\policy\planned_values.yml"
 }
+
