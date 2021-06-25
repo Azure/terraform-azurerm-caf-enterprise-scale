b4dc98b076b1f1f96dfa663532b2ba219410d832
J0hn-B <v-jbenakis@microsoft.com>
2021-06-25 15:47:46 +0100

    update pwsh scoop bucket -  add test-path

--- tests/scripts/opa-values-generator.ps1
+++ tests/scripts/opa-values-generator.ps1
@@ -10,15 +10,30 @@
 # # Parameters
 $PLAN_NAME = "terraform-plan"
 
+# # #? Run a local test against a different module configuration:
+# # #* Update the path to run the tests on a different folder (example: ../deployment_2)
+# # #* Copy paste the variables.tf file from deployment folder and adjust your main.tf
+###############################################
+# # #* Path of the tested _es terraform module
+$MODULE_PATH = "../deployment"
+###############################################
+
 
 # Install Scoop
-if (Get-command -name scoop) {
+if (Get-command -name scoop -ErrorAction SilentlyContinue) {
     Write-Output "==> Scoop exists, skip install"
     scoop --version
+    scoop update
 }
 else {
+    Write-Output "`n"
+    Write-Output "==> To run Conftest tests on Windows, some utilities need to be installed with Scoop"
     Write-Output "==> To install Scoop on Windows, run this command from a new terminal:"
+    Write-Output "`n"
     Write-Output "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https:\\get.scoop.sh')"
+    Write-Output "`n"
+    Write-Output "==> After installing Scoop, run: .\opa-values-generator.ps1"
+    Write-Output "`n"
     exit
 }
 
@@ -59,17 +74,13 @@ if (Get-command -name conftest -ErrorAction SilentlyContinue) {
 }
 else {
     Write-Output "==> Install conftest on Windows..."
-    scoop bucket add instrumenta https:\\github.com\instrumenta\scoop-instrumenta
+    scoop bucket add instrumenta https://github.com/instrumenta/scoop-instrumenta
     scoop install conftest
 }
 
-# # #? Run a local test against a different module configuration:
-# # #* Update the path to run the tests on a different folder (example: ../deployment_2)
-# # #* Copy paste the variables.tf file from deployment folder and adjust your main.tf
-###############################################
-# # #* Path of the tested _es terraform module
-$MODULE_PATH = "../deployment"
-###############################################
+
+
+if (-not ($MODULE_PATH | Test-Path)) { Throw "The directory does not exist, check path on .\opa-values-generator.ps1 :line 18" }
 
 Write-Output "==> Change to the module root directory..."
 Set-Location $MODULE_PATH
@@ -123,14 +134,20 @@ conftest test "$PLAN_NAME.json" -p ..\opa\policy\policy_assignments.rego -d ..\o
 
 $CONFIRM = Read-Host "Do you want to prepare files for repository (y/n)?"
 if ($CONFIRM -eq 'y') {
+    Write-Output "`n"
     Remove-Item -Path .\$PLAN_NAME.json
     Write-Output "$PLAN_NAME.json has been removed from your root module"
+    Write-Output "`n"
     Remove-Item -Path ..\opa\policy\planned_values.yml
     Write-Output "planned_values.yml has been removed from your \opa\policy\ directory"
+    Write-Output "`n"
 }
 else {
     Write-Warning -Message "$PLAN_NAME.json  can contain sensitive data"
     Write-Warning -Message  "Exposing $PLAN_NAME.json in a repository can cause security breach"
+    Write-Output "`n"
     Write-Output "From within your terraform root module: conftest test $PLAN_NAME.json -p ..\opa\policy\  -d ..\opa\policy\planned_values.yml"
+    Write-Output "`n"
 }
+
 
