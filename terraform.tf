542dfc7c23368a07b9696ddb6efcca1852af8abe
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-07-06 08:25:42 +0100

    Remove provider declarations

--- terraform.tf
+++ terraform.tf
@@ -4,24 +4,14 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.63.0"
+      configuration_aliases = [
+        azurerm.connectivity,
+        azurerm.management,
+      ]
     }
     time = {
       source  = "hashicorp/time"
       version = ">= 0.7.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-provider "azurerm" {
-  alias = "management"
-  features {}
-}
-
-provider "azurerm" {
-  alias = "connectivity"
-  features {}
 }
