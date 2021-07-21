a1748d618b00793cf891747a430f5735f7e7c137
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-07-21 19:51:15 +0100

    Further improve logic to generate outputs

--- tests/deployment/outputs.tf
+++ tests/deployment/outputs.tf
@@ -5,29 +5,14 @@
 
 output "resource_ids" {
   value = {
-    test_root_id_1 = {
-      for resource_type, resource_instances in module.test_root_id_1 :
-      resource_type => {
-        for resource_name, resource_configs in resource_instances :
-        resource_name => keys(resource_configs)
-      }
-    }
-    test_root_id_2 = {
-      for resource_type, resource_instances in module.test_root_id_2 :
-      resource_type => {
-        for resource_name, resource_configs in resource_instances :
-        resource_name => keys(resource_configs)
-      }
-    }
-    test_root_id_3 = {
-      for resource_type, resource_instances in module.test_root_id_3 :
-      resource_type => {
-        for resource_name, resource_configs in resource_instances :
-        resource_name => keys(resource_configs)
-      }
-    }
-    test_root_id_3_lz1 = {
-      for resource_type, resource_instances in module.test_root_id_3_lz1 :
+    for module_name, module_output in {
+      test_root_id_1     = module.test_root_id_1
+      test_root_id_2     = module.test_root_id_2
+      test_root_id_3     = module.test_root_id_3
+      test_root_id_3_lz1 = module.test_root_id_3_lz1
+    } :
+    module_name => {
+      for resource_type, resource_instances in module_output :
       resource_type => {
         for resource_name, resource_configs in resource_instances :
         resource_name => keys(resource_configs)
