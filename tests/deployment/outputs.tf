623d32d91242b6bac1665c9620acd0f0a7527c6b
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-07-21 19:44:32 +0100

    Improve logic to generate outputs

--- tests/deployment/outputs.tf
+++ tests/deployment/outputs.tf
@@ -6,59 +6,31 @@
 output "resource_ids" {
   value = {
     test_root_id_1 = {
-      for key, value in module.test_root_id_1 :
-      key => {
-        enterprise_scale = try(keys(value.enterprise_scale), [])
-        level_1          = try(keys(value.level_1), [])
-        level_2          = try(keys(value.level_2), [])
-        level_3          = try(keys(value.level_3), [])
-        level_4          = try(keys(value.level_4), [])
-        level_5          = try(keys(value.level_5), [])
-        level_6          = try(keys(value.level_6), [])
-        management       = try(keys(value.management), [])
-        connectivity     = try(keys(value.connectivity), [])
+      for resource_type, resource_instances in module.test_root_id_1 :
+      resource_type => {
+        for resource_name, resource_configs in resource_instances :
+        resource_name => keys(resource_configs)
       }
     }
     test_root_id_2 = {
-      for key, value in module.test_root_id_2 :
-      key => {
-        enterprise_scale = try(keys(value.enterprise_scale), [])
-        level_1          = try(keys(value.level_1), [])
-        level_2          = try(keys(value.level_2), [])
-        level_3          = try(keys(value.level_3), [])
-        level_4          = try(keys(value.level_4), [])
-        level_5          = try(keys(value.level_5), [])
-        level_6          = try(keys(value.level_6), [])
-        management       = try(keys(value.management), [])
-        connectivity     = try(keys(value.connectivity), [])
+      for resource_type, resource_instances in module.test_root_id_2 :
+      resource_type => {
+        for resource_name, resource_configs in resource_instances :
+        resource_name => keys(resource_configs)
       }
     }
     test_root_id_3 = {
-      for key, value in module.test_root_id_3 :
-      key => {
-        enterprise_scale = try(keys(value.enterprise_scale), [])
-        level_1          = try(keys(value.level_1), [])
-        level_2          = try(keys(value.level_2), [])
-        level_3          = try(keys(value.level_3), [])
-        level_4          = try(keys(value.level_4), [])
-        level_5          = try(keys(value.level_5), [])
-        level_6          = try(keys(value.level_6), [])
-        management       = try(keys(value.management), [])
-        connectivity     = try(keys(value.connectivity), [])
+      for resource_type, resource_instances in module.test_root_id_3 :
+      resource_type => {
+        for resource_name, resource_configs in resource_instances :
+        resource_name => keys(resource_configs)
       }
     }
     test_root_id_3_lz1 = {
-      for key, value in module.test_root_id_3_lz1 :
-      key => {
-        enterprise_scale = try(keys(value.enterprise_scale), [])
-        level_1          = try(keys(value.level_1), [])
-        level_2          = try(keys(value.level_2), [])
-        level_3          = try(keys(value.level_3), [])
-        level_4          = try(keys(value.level_4), [])
-        level_5          = try(keys(value.level_5), [])
-        level_6          = try(keys(value.level_6), [])
-        management       = try(keys(value.management), [])
-        connectivity     = try(keys(value.connectivity), [])
+      for resource_type, resource_instances in module.test_root_id_3_lz1 :
+      resource_type => {
+        for resource_name, resource_configs in resource_instances :
+        resource_name => keys(resource_configs)
       }
     }
   }
