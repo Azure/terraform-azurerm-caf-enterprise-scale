f990e49cf3798583882d0907e2715c51a2afc720
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2021-08-03 09:47:04 +0100

    Add connectivity and identity to parameters and enforcement_mode

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ module "management_group_archetypes" {
   template_file_variables = local.template_file_variables
   default_location        = local.default_location
   enforcement_mode = merge(
+    try(module.connectivity_resources.configuration.archetype_config_overrides[basename(each.key)].enforcement_mode, null),
     try(module.identity_resources.configuration.archetype_config_overrides[basename(each.key)].enforcement_mode, null),
     try(module.management_resources.configuration.archetype_config_overrides[basename(each.key)].enforcement_mode, null),
   )
