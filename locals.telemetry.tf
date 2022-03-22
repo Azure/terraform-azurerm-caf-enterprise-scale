3b9ef47aa8720df85531fc7eb9bc9ccb78162d2f
Matt White <matt.white@microsoft.com>
2022-03-22 15:06:33 +0000

    Logic to find available MG

--- locals.telemetry.tf
+++ locals.telemetry.tf
@@ -7,10 +7,10 @@ locals {
   telem_puid = "36dcde81-8c33-4da0-8dc3-265381502ccb"
 
   # Version of the module
-  telem_version = "1.2.0"
+  telem_module_version = "1.2.0"
 }
 
-# The following locals are used to create the bitfield data dependent on the module configuration
+# The following locals are used to create the bitfield data, dependent on the module configuration
 locals {
   # Bitfield bit 1 (LSB): Is configure management resources set?
   telem_configure_management_resources = var.configure_management_resources ? 1 : 0
@@ -31,9 +31,21 @@ locals {
 # The following locals calculate the telemetry bitfield by summiung thhe above locals and then representing as hexadecimal
 locals {
   telem_bitfield_denery = local.telem_configure_management_resources + local.telem_configure_connectivity_resources + local.telem_configure_identity_resources + local.telem_deploy_core_landing_zones + local.telem_deploy_demo_landing_zones
-  telem_bitfield_hex = format("%x", local.telem_bitfield_denery)
+  telem_bitfield_hex    = format("%x", local.telem_bitfield_denery)
 }
 
+# This construicts the ARM deployment name that is used for the telemetry.
+# We shouldn't ever hit the 64 character limit but use substr just in case
 locals {
-  telem_arm_deployment_name = "puid-" + local.telem_puid + "-" + local.telem_version + "-" + local.telem_bitfield_hex + "-" + random_id.telemetry_id.hex
+  telem_arm_deployment_name = substr("puid-${local.telem_puid}-${local.telem_module_version}-${local.telem_bitfield_hex}-${random_id.telemetry_id.hex}", 0, 64)
+}
+
+# This determines the management group to which we target the deployment
+# First we try the root_id, then we try the first (by alphanumeric sort) management group in level_1
+# If neither of those exist, we give up :)
+locals {
+  telem_root_id_management_group_created = lookup(azurerm_management_group.level_1, local.root_id, null)
+  telem_first_level_1_management_group   = count(keys(azurerm_management_group.level_1)) > 0 ? sort(keys(azurerm_management_group.level_1))[0] : null
+  telem_deployment_enabled = (telem_root_id_management_group_created != null || telem_first_level_1_management_group != null) && !var.disable_telemetry
+  telem_deployment_scope = coalesce(telem_root_id_management_group_created, telem_first_level_1_management_group)
 }
--- resources.telemetry.tf
+++ resources.telemetry.tf
@@ -1,12 +1,14 @@
 # The following random id is created once per module instantiation and is appended to the teleletry deployment name
 resource "random_id" "telemetry_id" {
+  count       = var.disable_telemetry ? 0 : 1
   byte_length = 6
 }
 
 resource "azurerm_management_group_template_deployment" "telemetry" {
+  count                 = local.telem_deployment_enabled ? 1 : 0
   name                  = local.telem_arm_deployment_name
   location              = var.default_location
-  management_group_name = local.root_id
+  management_group_name = local.telem_deployment_scope
   template_content      = <<TEMPLATE
 {
   "$schema": "https://schema.management.azure.com/schemas/2019-08-01/managementGroupDeploymentTemplate.json#"
@@ -24,6 +26,6 @@ resource "azurerm_management_group_template_deployment" "telemetry" {
 TEMPLATE
 
   depends_on = [
-      azurerm_management_group.level_1[local.root_id]
+    azurerm_management_group.level_1[local.telem_deployment_scope]
   ]
 }
