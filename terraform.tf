e428209dd84e1ed6350dc7ac129ae9803829e763
Matt White <matt.white@microsoft.com>
2022-03-22 11:54:08 +0000

    Fix required_providers

--- terraform.tf
+++ terraform.tf
@@ -13,10 +13,10 @@ terraform {
       source  = "hashicorp/time"
       version = ">= 0.7.0"
     }
-  }
-  random = {
-    source = "hashicorp/random"
-    version = ">= 3.1.0"
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
   }
 
   required_version = ">= 0.15"
