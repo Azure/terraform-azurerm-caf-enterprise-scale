bf05444866f2b90bc01f597d5b7719ca8ff85539
Matt White <matt.white@microsoft.com>
2022-03-29 12:02:41 +0100

    Create azurerm `< v3.0.0` constraint

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.96.0"
+      version = ">= 2.96.0, < 3.0.0"
       configuration_aliases = [
         azurerm.connectivity,
         azurerm.management,
