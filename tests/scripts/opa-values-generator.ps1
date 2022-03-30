b5a5bea17f675e36c53702e7874284d5d3fcda02
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2022-03-30 21:17:06 +0100

    Update file naming

--- tests/scripts/opa-values-generator.ps1
+++ tests/scripts/opa-values-generator.ps1
@@ -55,7 +55,7 @@ foreach ($MODULE_PATH in $MODULE_PATHS) {
     if (-not ($MODULE_PATH | Test-Path)) { Throw "The directory does not exist, check entries in MODULE_PATHS variable on .\opa-values-generator.ps1 :line 18" }
 
     $TF_PLAN_OUT = "$MODULE_PATH/terraform_plan"
-    $PLANNED_VALUES = "$MODULE_PATH/planned_values"
+    $BASELINE_VALUES = "$MODULE_PATH/baseline_values"
     $MODULE_NAME = Split-Path $MODULE_PATH -Leaf
 
     Write-Output "==> ($MODULE_NAME) - Change to the module root directory..."
@@ -79,7 +79,7 @@ foreach ($MODULE_PATH in $MODULE_PATHS) {
     Write-Output "==> ($MODULE_NAME) - Removing the original plan..."
     Remove-Item -Path "$TF_PLAN_OUT"
 
-    Write-Output "==> ($MODULE_NAME) - Saving planned values to planned_values.json..."
+    Write-Output "==> ($MODULE_NAME) - Saving planned values to baseline_values.json..."
     Get-Content -Path "$TF_PLAN_OUT.json" |
     # Extract the `.planned_values` to reduce file size
     jq '.planned_values' |
@@ -89,29 +89,29 @@ foreach ($MODULE_PATH in $MODULE_PATHS) {
     # This appears to be a "bug" in Terraform, but any fix
     # would only work for future releases, hence the fix below.
     jq '(.root_module.child_modules[]?.child_modules // []) |= sort_by(.address)' |
-    Out-File -FilePath "$PLANNED_VALUES.json"
+    Out-File -FilePath "$BASELINE_VALUES.json"
 
     #  Run OPA Tests
     Set-Location $MODULE_PATH
     Write-Output "==> ($MODULE_NAME) - Running conftest..."
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_management_group resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/management_groups.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/management_groups.rego -d "$BASELINE_VALUES.json"
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_policy_definition resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_definitions.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_definitions.rego -d "$BASELINE_VALUES.json"
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_policy_set_definition resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_set_definitions.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_set_definitions.rego -d "$BASELINE_VALUES.json"
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_policy_assignment resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_assignments.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/policy_assignments.rego -d "$BASELINE_VALUES.json"
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_role_definition resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/role_definitions.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/role_definitions.rego -d "$BASELINE_VALUES.json"
 
     Write-Output "==> ($MODULE_NAME) - Testing azurerm_role_assignment resources..."
-    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/role_assignments.rego -d "$PLANNED_VALUES.json"
+    conftest test "$TF_PLAN_OUT.json" -p ../../opa/policy/role_assignments.rego -d "$BASELINE_VALUES.json"
 
     # Remove comments and $CONFIRM parameter for CMD prompt.
     # $CONFIRM = Read-Host "Do you want to prepare files for repository (y/n)?"
@@ -125,7 +125,7 @@ foreach ($MODULE_PATH in $MODULE_PATHS) {
         Write-Warning -Message "($MODULE_NAME) - $TF_PLAN_OUT.json  can contain sensitive data"
         Write-Warning -Message "($MODULE_NAME) - Exposing $TF_PLAN_OUT.json in a repository can cause security breach"
         Write-Output "`n"
-        Write-Output "($MODULE_NAME) - From within your terraform root module: conftest test $TF_PLAN_OUT.json -p ../../opa/policy/  -d $PLANNED_VALUES.json"
+        Write-Output "($MODULE_NAME) - From within your terraform root module: conftest test $TF_PLAN_OUT.json -p ../../opa/policy/  -d $BASELINE_VALUES.json"
         Write-Output "`n"
     }
 
