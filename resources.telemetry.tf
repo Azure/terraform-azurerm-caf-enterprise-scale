8edb4b2f1660aa532ecd0f5b147c6c7cf2a0ceff
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2022-04-01 21:06:47 +0100

    Use locals, not vars

--- resources.telemetry.tf
+++ resources.telemetry.tf
@@ -1,6 +1,6 @@
 # The following random id is created once per module instantiation and is appended to the teleletry deployment name
 resource "random_id" "telem" {
-  count       = var.disable_telemetry ? 0 : 1
+  count       = local.disable_telemetry ? 0 : 1
   byte_length = 4
 }
 
