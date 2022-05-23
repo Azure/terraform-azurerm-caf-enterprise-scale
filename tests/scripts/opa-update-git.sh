e42fad5c716031d82241648136a40a1fa111cc0e
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2022-05-23 17:16:00 +0100

    Add new PR logic

--- tests/scripts/opa-update-git.sh
+++ tests/scripts/opa-update-git.sh
@@ -6,11 +6,30 @@ set -e
 # - Run git commands to merge changes to branch
 #
 
-REPOSITORY_URI="${SYSTEM_PULLREQUEST_SOURCEREPOSITORYURI:-$BUILD_REPOSITORY_URI}"
-SOURCE_BRANCH="${SYSTEM_PULLREQUEST_SOURCEBRANCH:-$BUILD_SOURCEBRANCH}"
+echo "==> Checking pull request..."
+# Consider adding input variable to set PR_NUMBER for manual trigger
+PR_NUMBER="$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"
+if [ ${#PR_NUMBER} -eq 0 ]; then
+    echo "ERROR: Unable to find pull request details. Please check this workflow was triggered from a valid pull request."
+    exit 1
+fi
 
-echo "==> Test GH CLI auth status"
-gh auth login
+echo "==> Get details for pull request $PR_NUMBER..."
+PR_RESPONSE=$(curl https://api.github.com/repos/$BUILD_REPOSITORY_NAME/pulls/$PR_NUMBER | jq -c)
+REPOSITORY_URI=$(echo "$PR_RESPONSE" | jq -r ".head.repo.html_url")
+SOURCE_BRANCH=$(echo "$PR_RESPONSE" | jq -r ".head.ref")
+if [ ${#REPOSITORY_URI} -gt 0 ]; then
+    echo "Found PR repository: $REPOSITORY_URI"
+else
+    echo "ERROR: Unable to find REPOSITORY_URI for pull request."
+    exit 1
+fi
+if [ ${#SOURCE_BRANCH} -gt 0 ]; then
+    echo "Found PR source branch: $SOURCE_BRANCH"
+else
+    echo "ERROR: Unable to find SOURCE_BRANCH for pull request."
+    exit 1
+fi
 
 echo "==> Set git config..."
 git config user.name azure-devops
@@ -27,7 +46,7 @@ STATUS_LOG=$(git status --short | grep baseline_values.json)
 if [ ${#STATUS_LOG} -gt 0 ]; then
     git add --all ./tests/modules/*/baseline_values.json
 else
-    echo "No changes found to add."
+    echo "No changes to add."
 fi
 
 echo "==> Print git diff..."
@@ -38,7 +57,7 @@ COMMIT_LOG=$(git diff --cached)
 if [ ${#COMMIT_LOG} -gt 0 ]; then
     git commit --message "Add updates to baseline_values.json"
 else
-    echo "No changes found to commit."
+    echo "No changes to commit."
 fi
 
 echo "==> Push changes..."
