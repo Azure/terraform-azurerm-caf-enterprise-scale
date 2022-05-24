7a714dd35280c7df4fc1a72b7a41a43bf4e7e898
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2022-05-24 11:58:31 +0100

    Update workflow

--- tests/scripts/opa-update-git.sh
+++ tests/scripts/opa-update-git.sh
@@ -6,31 +6,6 @@ set -e
 # - Run git commands to merge changes to branch
 #
 
-echo "==> Checking pull request..."
-# Consider adding input variable to set PR_NUMBER for manual trigger
-PR_NUMBER="$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"
-if [ ${#PR_NUMBER} -eq 0 ]; then
-    echo "ERROR: Unable to find pull request details. Please check this workflow was triggered from a valid pull request."
-    exit 1
-fi
-
-echo "==> Get details for pull request $PR_NUMBER..."
-PR_RESPONSE=$(curl https://api.github.com/repos/$BUILD_REPOSITORY_NAME/pulls/$PR_NUMBER | jq -c)
-REPOSITORY_URI=$(echo "$PR_RESPONSE" | jq -r ".head.repo.html_url")
-SOURCE_BRANCH=$(echo "$PR_RESPONSE" | jq -r ".head.ref")
-if [ ${#REPOSITORY_URI} -gt 0 ]; then
-    echo "Found PR repository: $REPOSITORY_URI"
-else
-    echo "ERROR: Unable to find REPOSITORY_URI for pull request."
-    exit 1
-fi
-if [ ${#SOURCE_BRANCH} -gt 0 ]; then
-    echo "Found PR source branch: $SOURCE_BRANCH"
-else
-    echo "ERROR: Unable to find SOURCE_BRANCH for pull request."
-    exit 1
-fi
-
 echo "==> Set git config..."
 git config user.name azure-devops
 git config user.email azuredevops@microsoft.com
