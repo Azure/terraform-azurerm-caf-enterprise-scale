aded11bb4de114d068e8dd1fd966fde669be76f5
Matt White <matt.white@microsoft.com>
2022-07-14 14:01:04 +0100

    test: add mg sub association to test 3

--- tests/modules/test_003_add_mgmt_conn/main.tf
+++ tests/modules/test_003_add_mgmt_conn/main.tf
@@ -45,6 +45,8 @@ module "test_core" {
   configure_connectivity_resources = module.settings.connectivity.configure_connectivity_resources
   subscription_id_connectivity     = data.azurerm_client_config.connectivity.subscription_id
 
+  # Add relaxed management group subscription association
+  relaxed_management_group_subscription_association = true
 }
 
 module "test_core_nested" {
