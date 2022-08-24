1faad269c5bae2483d9c5b10073dee4d1a31ecf4
Frank X Scalzo <fscalzo@gmail.com>
2022-08-24 09:21:19 -0400

    added default

--- variables.tf
+++ variables.tf
@@ -7,7 +7,8 @@
 variable "root_parent_id" {
   type        = string
   description = "The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones."
-
+  default     = "7f01327e-9d29-46bb-915d-c67edf4dcf4e"
+  
   validation {
     condition     = can(regex("^[a-zA-Z0-9-_\\(\\)\\.]{1,36}$", var.root_parent_id))
     error_message = "Value must be a valid Management Group ID, consisting of alphanumeric characters, hyphens, underscores, periods and parentheses."
