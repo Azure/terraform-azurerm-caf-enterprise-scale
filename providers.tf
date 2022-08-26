ab8475f63550e47f2cf29f4e01af72dd64ec9a5a
Frank X Scalzo <fscalzo@gmail.com>
2022-08-26 09:17:48 -0400

    minor change to providers

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
-# The following locals are used to establish the subscription_id for each provider
+/* # The following locals are used to establish the subscription_id for each provider
 locals {
   subscription_id_management   = coalesce(var.subscription_id_management, data.azurerm_client_config.current.subscription_id)
   subscription_id_connectivity = coalesce(var.subscription_id_connectivity, local.subscription_id_management)
 }
-
+*/
 provider "azurerm" {
   tenant_id = "7f01327e-9d29-46bb-915d-c67edf4dcf4e"
   subscription_id = "07f192e5-05e9-4c31-91a4-eabbd2be9099"
@@ -11,8 +11,8 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  tenant_id = "7f01327e-9d29-46bb-915d-c67edf4dcf4e"
   alias = "connectivity"
+  tenant_id = "7f01327e-9d29-46bb-915d-c67edf4dcf4e"
   features {}
 
   subscription_id = local.subscription_id_connectivity
