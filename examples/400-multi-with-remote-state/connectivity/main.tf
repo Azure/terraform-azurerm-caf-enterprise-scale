3b1a9c1b4fce22b8ca9ad5c5cd62fb0d29305fb2
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2022-12-22 11:56:33 +0000

    Add remote state example

---
+++ examples/400-multi-with-remote-state/connectivity/main.tf
@@ -0,0 +1,54 @@
+# Configure Terraform to set the required AzureRM provider
+# version and features{} block
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.19.0"
+    }
+  }
+  backend "local" {
+    path = "./connectivity.tfstate"
+  }
+}
+
+# Define the provider configuration
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id_connectivity
+}
+
+# Get the current client configuration from the AzureRM provider
+
+data "azurerm_client_config" "current" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide the connectivity configuration
+
+module "alz" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "3.0.0"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  # Base module configuration settings
+  root_parent_id = data.azurerm_client_config.current.tenant_id
+  root_id        = var.root_id
+
+  # Disable creation of the core management group hierarchy
+  # as this is being created by the core module instance
+  deploy_core_landing_zones = false
+
+  # Configuration settings for connectivity resources
+  deploy_connectivity_resources    = true
+  configure_connectivity_resources = local.configure_connectivity_resources
+  subscription_id_connectivity     = var.subscription_id_connectivity
+
+}
---
+++ examples/400-multi-with-remote-state/management/main.tf
@@ -0,0 +1,54 @@
+# Configure Terraform to set the required AzureRM provider
+# version and features{} block
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.19.0"
+    }
+  }
+  backend "local" {
+    path = "./management.tfstate"
+  }
+}
+
+# Define the provider configuration
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id_management
+}
+
+# Get the current client configuration from the AzureRM provider
+
+data "azurerm_client_config" "current" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide the connectivity configuration.
+
+module "alz" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "3.0.0"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  # Base module configuration settings
+  root_parent_id = data.azurerm_client_config.current.tenant_id
+  root_id        = var.root_id
+
+  # Disable creation of the core management group hierarchy
+  # as this is being created by the core module instance
+  deploy_core_landing_zones = false
+
+  # Configuration settings for management resources
+  deploy_management_resources    = true
+  configure_management_resources = local.configure_management_resources
+  subscription_id_management     = var.subscription_id_management
+
+}
