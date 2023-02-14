884961f8abec204d7c8cbb9f57ac8baa7dcf35df
Jared Holgate <jaredholgate@microsoft.com>
2023-02-14 12:26:49 +0000

    Update tests

--- tests/modules/test_001_baseline/main.tf
+++ tests/modules/test_001_baseline/main.tf
@@ -20,7 +20,4 @@ module "test_core" {
 
   # Test disable_telemetry
   disable_telemetry = true
-
-  # Disable default non-compliance messages
-  policy_compliance_message_default_enabled = false
 }
