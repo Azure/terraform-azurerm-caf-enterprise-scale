bd3c9f37e2dca61852d45c5db2ff1cd10f3eeda6
Jared Holgate <jaredholgate@microsoft.com>
2023-04-27 21:03:36 +0100

    Fix for 10 character name issue

--- .github/scripts/Invoke-LibraryUpdatePolicyAssignments.ps1
+++ .github/scripts/Invoke-LibraryUpdatePolicyAssignments.ps1
@@ -111,6 +111,10 @@ foreach($sourcePolicyAssignmentFile in $sourcePolicyAssignmentFiles)
             $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value = $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value.Replace("`${private_dns_zone_prefix}/providers/Microsoft.Network/privateDnsZones/", "`${private_dns_zone_prefix}")
             $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value = $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value.Replace("privatelink.batch.azure.com", "privatelink.`${connectivity_location}.batch.azure.com")
         }
+        if($parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value.StartsWith("`${temp}"))
+        {
+            $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value = $parsedAssignments[$parsedAssignment.name].json.properties.parameters.($propertyName).value.Replace("`${temp}", "`${root_scope_id}")
+        }
     }
 }
 
