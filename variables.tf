668c23a3cb3d67b71e546f82a77ee9968f341e62
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-04-28 15:02:53 +0100

    docs: terraform-docs

--- variables.tf
+++ variables.tf
@@ -466,7 +466,6 @@ e.g.
       archetype_id = "root"
       enforcement_mode = {
         "Example-Policy-Assignment" = true,
-
       }
       parameters = {
         Example-Policy-Assignment = {
@@ -541,7 +540,7 @@ variable "custom_landing_zones" {
 If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
 Although the object type for this input variable is set to `any`, the expected object is based on the following structure:
 
-```hcl
+```terraform
 variable "custom_landing_zones" {
   type = map(
     object({
@@ -557,7 +556,7 @@ variable "custom_landing_zones" {
   )
 ```
 
-> INFORMATION: The decision not to hard code the structure in the input variable `type` is by design, as it allows Terraform to handle the input as a dynamic object type.
+The decision not to hard code the structure in the input variable `type` is by design, as it allows Terraform to handle the input as a dynamic object type.
 This was necessary to allow the `parameters` value to be correctly interpreted.
 Without this, Terraform would throw an error if each parameter value wasn't a consistent type, as it would incorrectly identify the input as a `tuple` which must contain consistent type structure across all entries.
 
@@ -570,11 +569,11 @@ The main object parameters are `display_name`, `parent_management_group_id`, `su
 
 - `subscription_ids` is an object containing a list of Subscription IDs to assign to the current Management Group.
 
-- `archetype_config` is used to configure the configuration applied to each Management Group. This object must contain valid entries for the `archetype_id` `parameters`, and `access_control` attributes. For more information about how to populate this object, please refer to the [Working with the `archetype config` object][archetype_config_object] section on our Wiki.
+- `archetype_config` is used to configure the configuration applied to each Management Group. This object must contain valid entries for the `archetype_id` `parameters`, and `access_control` attributes.
 
-The following example shows how you would add a simple Management Group `${root_id}-customer-corp` under the `${root_id}-landing-zones` Management Group, where `root_id = "myorg"` and using the [`default_empty`][using_default_empty] archetype definition.
+The following example shows how you would add a simple Management Group under the `myorg-landing-zones` Management Group, where `root_id = "myorg"` and using the default_empty archetype definition.
 
-```hcl
+```terraform
   custom_landing_zones = {
     myorg-customer-corp = {
       display_name               = "MyOrg Customer Corp"
