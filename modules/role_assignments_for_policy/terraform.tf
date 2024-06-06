4dda54884db042aeebb96eefb690e7a62b7dfa87
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-06-06 19:13:21 +0100

    fix: terraform required providers

--- modules/role_assignments_for_policy/terraform.tf
+++ modules/role_assignments_for_policy/terraform.tf
@@ -5,6 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.84"
     }
-    required_version = "~> 1.7"
   }
+  required_version = "~> 1.7"
 }
--- terraform.tf
+++ terraform.tf
@@ -22,6 +22,5 @@ terraform {
       version = "~> 1.7"
     }
   }
-
   required_version = "~> 1.7"
 }
