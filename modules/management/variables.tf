cfb662575bf2daf17929a4d76bef4ad040403e02
Jared Holgate <jaredholgate@microsoft.com>
2024-07-16 15:53:10 +0100

    Fix dcr custom config (#1037)

--- modules/management/variables.tf
+++ modules/management/variables.tf
@@ -136,7 +136,7 @@ variable "custom_settings_by_resource_type" {
   default     = {}
 
   validation {
-    condition     = can([for k in keys(var.custom_settings_by_resource_type) : contains(["azurerm_resource_group", "azurerm_log_analytics_workspace", "azurerm_log_analytics_solution", "azurerm_automation_account", "azurerm_log_analytics_linked_service"], k)]) || var.custom_settings_by_resource_type == {}
+    condition     = can([for k in keys(var.custom_settings_by_resource_type) : contains(["azurerm_resource_group", "azurerm_log_analytics_workspace", "azurerm_log_analytics_solution", "azurerm_automation_account", "azurerm_log_analytics_linked_service", "azurerm_data_collection_rule"], k)]) || var.custom_settings_by_resource_type == {}
     error_message = "Invalid key specified. Please check the list of allowed resource types supported by the management module for caf-enterprise-scale."
   }
 }
