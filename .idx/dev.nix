12b47d592c1104de064638203ca09b176d1a2fb3
ipartner <ipartner@gmail.com>
2025-04-16 20:46:32 +0000

    feat/add-source

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,12 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.go
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
+    pkgs.azure-cli
   ];
 
   # Sets environment variables in the workspace
