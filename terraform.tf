05b4590053ee26b402365ab22044b1b2b08ead30
ipartner <ipartner@gmail.com>
2025-04-16 21:41:08 +0000

    feat/add-source

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 # Configure the minimum Terraform supported version by this module
 terraform {
-  required_version = "~> 1.7"
+  required_version = "~> 1.11.4"
 }
 
 # Configure the default backend for the Terraform State
