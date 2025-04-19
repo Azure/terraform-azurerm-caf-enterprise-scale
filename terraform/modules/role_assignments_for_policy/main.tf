7b801f465e9c6c9d692bbd0e17125f0c17a4be06
ipartner <ipartner@gmail.com>
2025-04-19 10:29:26 +0000

    feat/add-source

--- terraform/modules/role_assignments_for_policy/main.tf
+++ terraform/modules/role_assignments_for_policy/main.tf
@@ -1,15 +1,2 @@
-resource "azurerm_role_assignment" "for_policy" {
-  for_each = local.azurerm_role_assignments
-
-  # Special handling of OPTIONAL name to ensure consistent and correct
-  # mapping of Terraform state ADDR value to Azure Resource ID value.
-  name = each.value.name
-
-  # Mandatory resource attributes
-  scope        = each.value.scope
-  principal_id = each.value.principal_id
-
-  # Optional attributes
-  role_definition_name = lookup(each.value, "role_definition_name", null)
-  role_definition_id   = lookup(each.value, "role_definition_id", null)
-}
+# No resources deployed by this module so this file is here as an entry point only
+# Please navigate the variables, locals and outputs to see how the data model is generated from the inputs
